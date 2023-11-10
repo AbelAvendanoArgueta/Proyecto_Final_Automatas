528dcef532ab2f275025dd42f5ee8c54a24be8c1
Javier_Windows <javiergomz0509@gmail.com>
2023-11-09 23:28:32 -0600

    modifiqué el programa para que reconozca varios caminos

--- proyecto_final.py
+++ proyecto_final.py
@@ -19,7 +19,6 @@ class AutomataFinito:
         else:
             print("Error: Estado no válido.")
 
-    
     def imprimir_tabla_transiciones(self):
         print("\nTabla de Transiciones:")
         for estado_actual, transiciones in self.tabla_transiciones.items():
@@ -28,7 +27,7 @@ class AutomataFinito:
 
     def calcular_tamano_simbolo(self):
         tamaño_simbolo = []  # Inicializa la lista vacía
-        
+
         for i in range(1, len(self.estados) + 1):
             estado = str(i)
             tamaño_estado = 0  # Inicializa el tamaño del estado actual
@@ -37,46 +36,34 @@ class AutomataFinito:
                 for simbolo_entrada, estado_siguiente in transiciones.items():
                     tamaño_estado += len(simbolo_entrada)  # Suma el tamaño de cada símbolo
             tamaño_simbolo.append(tamaño_estado)
-        
+
         return tamaño_simbolo
 
-    def validar_cadena(self, cadena):
-        estado_actual = self.estado_inicial
-        estados_recorridos = [estado_actual]
-
-        # Crear un diccionario para almacenar las cadenas divididas según el tamaño del símbolo
-        cadenas_divididas = {}
-
-        # Inicializamos una variable para mantener un seguimiento de la posición en la cadena.
-        posicion = 0
-
-        # Iteramos a través de los estados y tamaños de símbolos para dividir la cadena
-        for estado, tamaño in zip(self.estados, self.calcular_tamano_simbolo()):
-            # Dividir la cadena en partes del tamaño correcto
-            cadenas_divididas[estado] = cadena[posicion:posicion + tamaño]
-            posicion += tamaño
-
-        for estado, subcadena in cadenas_divididas.items():
-            if estado in self.alfabeto:
-                if estado_actual in self.tabla_transiciones and subcadena in self.tabla_transiciones[estado_actual]:
-                    # La subcadena es válida y hay una transición válida desde el estado actual
-                    # al estado siguiente con la subcadena dada.
-                    estado_actual = self.tabla_transiciones[estado_actual][subcadena]
-                    estados_recorridos.append(estado_actual)
-                else:
-                    print(f"La cadena es inválida en el estado {estado_actual} con la subcadena '{subcadena}'.")
-                    return False, estados_recorridos
+    def validar_cadena(self, cadena, estado_actual, posicion):
+        if posicion == len(cadena):
+            if estado_actual in self.estados_aceptacion:
+                print("La cadena es válida.")
+                return True, [estado_actual]
             else:
-                print(f"La cadena contiene símbolos no válidos.")
-                return False, estados_recorridos
+                print(f"La cadena es inválida en el estado final {estado_actual}.")
+                return False, [estado_actual]
 
-        if estado_actual in self.estados_aceptacion:
-            # Verifica si el estado final es un estado de aceptación.
-            print(f"La cadena es válida.")
-            return True, estados_recorridos
-        else:
-            print(f"La cadena es inválida en el estado final {estado_actual}.")
-            return False, estados_recorridos
+        estados_recorridos = []
+
+        if estado_actual in self.tabla_transiciones:
+            for simbolo, estado_siguiente in self.tabla_transiciones[estado_actual].items():
+                if cadena[posicion:].startswith(simbolo):
+                    resultado, recorrido = self.validar_cadena(cadena, estado_siguiente, posicion + len(simbolo))
+                    if resultado:
+                        estados_recorridos.extend([estado_actual] + recorrido)
+                        return True, estados_recorridos
+                    else:
+                        print(f"La cadena es inválida en el estado {estado_actual} con la subcadena '{simbolo}'.")
+                        return False, estados_recorridos
+
+        print(f"La cadena es inválida en el estado {estado_actual} con la subcadena '{cadena[posicion:]}'.")
+        return False, [estado_actual]
+
 
 # Crear una instancia del autómata
 automata = AutomataFinito()
@@ -104,11 +91,11 @@ num_transiciones = int(input("Número de transiciones: "))
 
 for i in range(num_transiciones):
     estado_actual = input(f"\nTransición {i + 1} - Estado actual: ")
-    
+
     while True:
         # Solicita al usuario el símbolo de entrada
         simbolo_entrada = input(f"Transición {i + 1} - Símbolo de entrada: ")
-        
+
         # Verifica si todos los caracteres de la cadena están en el alfabeto
         if all(char in alfabeto for char in simbolo_entrada):
             # Si todos los caracteres son válidos, sal del bucle
@@ -116,9 +103,9 @@ for i in range(num_transiciones):
         else:
             # Si algunos caracteres no son válidos, muestra un mensaje de error
             print("Símbolo de entrada no válido. Los símbolos deben estar en el alfabeto: " + alfabeto)
-    
+
     estado_siguiente = input(f"Transición {i + 1} - Estado siguiente: ")
-    
+
     # Agrega la transición al autómata
     automata.agregar_transicion(estado_actual, simbolo_entrada, estado_siguiente)
 
@@ -138,9 +125,10 @@ while True:
         break
 
     # Validar la cadena y mostrar los recorridos
-    resultado, estados_recorridos = automata.validar_cadena(cadena)
+    resultado, estados_recorridos = automata.validar_cadena(cadena, automata.estado_inicial, 0)
+
     if resultado:
-        print("Estados recorridos:", " -> ".join(estados_recorridos),"\n")
-    
+        print("Estados recorridos:", " -> ".join(estados_recorridos), "\n")
+
     else:
-        print("Estados recorridos:", " -> ".join(estados_recorridos),"\n")
+        print("Estados recorridos:", " -> ".join(estados_recorridos), "\n")
